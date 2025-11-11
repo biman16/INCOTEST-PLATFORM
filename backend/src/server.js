5e2946a485c9d1c6335d9c196ec4ba8eb3882737
Biman Chandra Das <bimandas1611@gmail.com>
2025-11-11 16:17:23 +0530

    db configaration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
@@ -7,4 +8,14 @@ app.get("/health", (req, res)=>{
     res.status(200).json({message:"API is up and running."});
 })
 
-app.listen(ENV.PORT, ()=> console.log("Server running on port:", ENV.PORT))
+
+const serverStart = async() => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, ()=> console.log("Server running on port:", ENV.PORT))
+    } catch (error) {
+        console.error("💣 Error occured while connecting server.", error)
+    }
+};
+
+serverStart();
